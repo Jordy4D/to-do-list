cfa986803e55cc0e11a7d5ea90fe5bc472dfc948
JordanAdams_MacBook <jsadams27@gmail.com>
2024-05-30 20:34:21 -0400

    project init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+class ToDoList {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+    
+    getDueDate() {
+        //do stuff
+    }
+
+    getPriority() {
+        //do stuff
+    }
+
+    updateNotes() {
+        //do stuff
+    }
+
+}
+
+Class Project {
+    constructor()
+}
