0009194b2752d19c5bfcf3bd8e007eb97063da9d
JordanAdams_MacBook <jsadams27@gmail.com>
2024-06-14 08:10:17 -0400

    adds height to project list box and data value to projects (probably needs changed though)

--- src/project.js
+++ src/project.js
@@ -6,6 +6,7 @@ console.log('test Project')
 export class Project {
     constructor(name) {
         this.name = name;
+        this.dataName = name.toLowerCase();
         this.tasks = [];
     }
     
